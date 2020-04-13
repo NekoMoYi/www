eb0073c56fad5206bf74ee018b85cff3ab29cc18
moyiljx <1254846416@qq.com>
2020-04-13 17:13:50 +0800

    增加blog入口

--- src/views/home.vue
+++ src/views/home.vue
@@ -6,9 +6,30 @@
           <v-card-title>
             <div class="fill-width">
               <div class="text-uppercase font-italic font-weight-light subtitle-1">WELCOME</div>
-              <div class="text-right display-1 font-weight-thin">MoYi.ML</div>
+              <div class="text-right display-1 font-weight-thin">MOYI.ML</div>
             </div>
           </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12" sm="6" md="4">
+        <v-card color="#FFFFFFc0" style="backdrop-filter: blur(10px);">
+          <v-card-title>SITES</v-card-title>
+          <v-divider/>
+          <v-list color="transparent">
+            <v-list-item href="https://blog.moyi.ml/" target="_blank">
+              <v-list-item-avatar tile>
+                <v-img :src="require('@/assets/blog.png')" contain/>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>BLOG</v-list-item-title>
+                <v-list-item-subtitle>
+                  build with hexo
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
         </v-card>
       </v-col>
     </v-row>
