4f2a58baaa26ebe00a6af7ef4859e09521f6bb01
默一 <1254846416@qq.com>
2020-12-31 21:36:17 +0800

    use cdn

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
-import vuetify from '@/plugins/vuetify' // path to vuetify export
 import app from './app.vue'
 import router from './utils/router'
-import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  vuetify,
+  vuetify: window.Vuetify,
   render: h => h(app)
 }).$mount('#app')
